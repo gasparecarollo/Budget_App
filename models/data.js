8a85b8f0e092954adec166852e3d8da41934065a
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-10-23 23:15:13 -0400

    Added data.js

---
+++ models/data.js
@@ -0,0 +1,92 @@
+module.exports = [
+
+    {
+        id: 1122,
+        item_name: "Income",
+        amount: 1000,
+        date: "April 1, 2023",
+        from: "employer"
+    },
+
+    {
+        id: 1133,
+        item_name: "Taxes",
+        amount: -300,
+        date: "April 1, 2023",
+        from: "Uncle Sam"
+
+
+    },
+
+    {
+
+        id: 1144,
+        item_name: "Retirement",
+        amount: -200,
+        date: "April 1, 2023",
+        from: "Pension Plan"
+
+    },
+
+    {
+        id: 1155,
+        item_name: "Savings",
+        amount: -100,
+        date: "April 1, 2023",
+        from: "BankAccount"
+    },
+
+    {
+        id: 1166,
+        item_name: "Credit Card Payment",
+        amount: -100,
+        date: "April 1, 2023",
+        from: "Platinum Card"
+
+    },
+
+    {
+
+        id: 1177,
+        item_name: "Monthly Birthday Money from Aunt Tilda",
+        amount: 20,
+        date: "April 5, 2023",
+        from: "Aunt Tilda"
+
+    },
+    {
+        id: 1188,
+        item_name: "Coffee",
+        amount: -4,
+        date: "April 5, 2023",
+        from: "Dunkin Donuts"
+    },
+
+    {
+        id: 1199,
+        item_name: "Internet",
+        amount: -100,
+        date: "April 5, 2023",
+        from: "The Internet Company"
+
+    },
+
+    {
+
+        id: 1210,
+        item_name: "Groceries",
+        amount: -76,
+        date: "April 3, 2023",
+        from: "The Grossery Story"
+
+    },
+    {
+        id: 1221,
+        item_name: "Pet Food",
+        amount: -7,
+        date: "April 3, 2023",
+        from: "The Pet Store"
+    }
+
+
+]
